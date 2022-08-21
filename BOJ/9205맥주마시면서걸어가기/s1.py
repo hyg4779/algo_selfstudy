620a74bf4dfd7d86f1c29017959546f3e3f20063
hong <hyg4779@naver.com>
2022-08-21 23:58:23 +0900

    맥주마시면서걸어가기

--- BOJ/9205맥주마시면서걸어가기/s1.py
+++ BOJ/9205맥주마시면서걸어가기/s1.py
@@ -10,3 +10,19 @@ for tc in range(int(input())):
 
     visit = [0]*(n+1)
 
+    Q = deque([(home[0], home[1])])
+    while Q:
+        x, y = Q.popleft()
+
+        if abs(x-fest[0]) + abs(y-fest[1]) <=1000:
+            print('happy')
+            break
+
+        for i in range(n):
+            if not visit[i]:
+                r, c = graph[i]
+                if abs(x-r)+abs(y-c)<=1000:
+                    Q.append((r, c))
+                    visit[i] = 1
+    else:
+        print('sad')
