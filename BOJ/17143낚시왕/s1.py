152cbf68a9f4bee300f728ff25c1624a6822f709
hong <hyg4779@naver.com>
2022-08-30 23:40:13 +0900

    낚시왕 주석

--- BOJ/17143낚시왕/s1.py
+++ BOJ/17143낚시왕/s1.py
@@ -13,13 +13,14 @@ for i in range(k):
 ans = 0
 for j in range(m):
 
+    # 해당 위치 상어 잡기
     for i in range(n):
         if arr[i][j]:
             ans += arr[i][j][2]
             arr[i][j] = 0
             break
 
-
+    # 이동 된 상어 임시 배열
     new = [[0]*m for _ in range(n)]
     for r in range(n):
         for c in range(m):
@@ -29,6 +30,12 @@ for j in range(m):
                 nr, nc = r, c
                 speed = tmp
 
+                '''
+                상어가 이동 중 에 벽 또는 바닥 또는 수면을 찍고 되돌아 올 때
+                예를 들어 4가 너비라면
+                1 2 3 4 3 2  즉 (4-1)*2 만큼의 인덱스 이동이 있다
+                speed만큼 이동하는데 처음 시작을 1번 째로 두고 한다면 다음 위치는 아래와 같다
+                '''
                 if direct == 1 or direct == 2:
                     rot = 2*(n-1)
                     if direct == 1:
@@ -58,12 +65,12 @@ for j in range(m):
                         nr, nc, direct = r, tmp, 3
 
 
-
-                if new[nr][nc]:
-                    new[nr][nc] = max(new[nr][nc], (speed, direct, size), key=lambda x: x[2])
-                else:
-                    new[nr][nc] = (speed, direct, size)
-
+            # 이미 자리에 상어가 있다면 재배치
+            if new[nr][nc]:
+                new[nr][nc] = max(new[nr][nc], (speed, direct, size), key=lambda x: x[2])
+            else:
+                new[nr][nc] = (speed, direct, size)
+    # 새로 완성된 배열로 치환
     arr = new
 
 print(ans)
