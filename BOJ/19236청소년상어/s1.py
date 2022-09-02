c6a634f7b3f8681849f744bd4c89453b4c0a13de
hong <hyg4779@naver.com>
2022-09-03 00:46:05 +0900

    청소년 상어

--- BOJ/19236청소년상어/s1.py
+++ BOJ/19236청소년상어/s1.py
@@ -1,27 +1,33 @@
-arr = [[] for _ in range(4)]
-fishes = [0 for _ in range(17)]
+mat = [[[], [], [], []] for _ in range(4)]
+info = [0 for _ in range(17)]
 dp = [(-1, 0), (-1, -1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1)]
 
 # 물고기 번호와 방향을 담음
 for i in range(4):
     line = list(map(int, input().split()))
     for j in range(0, 8, 2):
-        arr.append([line[2*j], line[2*j+1]])
+
+        # 격자에 물고기 번호와 방향을 저장
+        mat[i][j//2] = [line[j], line[j+1]-1]
         # 물고기 번호 별 행열번호 저장
-        fishes[line[2*j]] = (i, j//2)
+        info[line[j]] = (i, j//2)
 
 
 # 상어 초기 시작
-first = arr[0][0]
+first = mat[0][0]
+
+# 위치, 방향
 shark = (0, 0, first[1])
+
+# 먹은 양
 answer = first[0]
-fishes[first[0]] = 0
 
 # 0, 0 위치 초기화
-arr[0][0] = -1
+info[first[0]] = 0
+mat[0][0] = -1
 
 
-def move():
+def move(arr, fishes, now, cnt):
     # 물고기 이동
     for num in range(1, 17):
         # 먹힌 물고기는 pass
@@ -34,7 +40,7 @@ def move():
 
         # 8방향 탐색
         for d in range(8):
-            # 다음 이동 방향향
+            # 다음 이동 방향
             head = (p+d)%8
             nr, nc = r + dp[head][0], c + dp[head][1]
 
@@ -43,11 +49,11 @@ def move():
                 # 해당 위치에 물고기가 있다면
                 if arr[nr][nc]:
                     # 해당 위치 물고기 번호
-                    tmp = arr[nr][nc]
+                    tmp_num, tmp_head = arr[nr][nc]
                     # 서로 위치 교환
                     arr[r][c], arr[nr][nc] = arr[nr][nc], (num, head)
                     # 현재 와 해당 위치 물고기의 위치 정보 수정
-                    fishes[num], fishes[tmp[0]] = (nr, nc), (r, c)
+                    fishes[num], fishes[tmp_num] = (nr, nc), (r, c)
 
                 # 빈 자리라면
                 else:
@@ -58,22 +64,32 @@ def move():
                 # 위치 교환 했다면 브레이크
                 # 위치 교환 못했다면 for문을 돌며 방향 탐색. 모든 방향 이동 불가면 제자리
 
-
-def eat():
     global answer
-    # 상어 이동
 
-    x, y, s = shark
-    flag = True
-    while 0 <= x < 4 and 0 <= y < 4:
-        x, y = x+dp[s][0], y+dp[s][1]
-        if arr[x][y]:
-            flag = False
+    # 상어 위치와 방향
+    x, y, s = now
+
+    n = 1
+    while True:
+        nx, ny = x+dp[s][0]*n, y+dp[s][1]*n
+        if 0 <= nx < 4 and 0 <= ny < 4:
+            if arr[nx][ny]:
+
+                size, next_s = arr[nx][ny]
+                arr[x][y], arr[nx][ny] = 0, -1
+
+                fishes[size] = 0
+                move([l[:] for l in arr], fishes[:], (nx, ny, next_s), cnt+size)
+                fishes[size] = (nx, ny)
+
+                arr[x][y], arr[nx][ny] = -1, (size, next_s)
+        else:
+            answer = max(answer, cnt)
+            break
+        n += 1
 
 
-
-move()
-eat()
+move(mat, info, shark, answer)
 
 
 print(answer)
