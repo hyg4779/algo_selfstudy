b4002e31eb72bdab6484aaf292fadedd735d0231
hong <hyg4779@naver.com>
2022-09-23 02:14:13 +0900

    구슬탈출2

--- BOJ/13460구슬탈출2/s1.py
+++ BOJ/13460구슬탈출2/s1.py
@@ -3,42 +3,74 @@ import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-arr = [list(input()) for _ in range(n)]
+field = [list(input()) for _ in range(n)]
 
 direct = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-red, blue, hole = [0, 0], [0, 0], [0, 0]
+rr, rc = 0, 0
+br, bc = 0, 0
 
-for a in range(1, n-1):
-    for b in range(1, m-1):
-        if arr[a][b]=='R':
-            red = [a, b]
-        elif arr[a][b]=='B':
-            blue = [a, b]
-        elif arr[a][b]=='O':
-            hole = [a, b]
+for i in range(1, n-1):
+    for j in range(1, m-1):
+        if field[i][j] == 'R':
+            rr, rc = i, j
+        elif field[i][j] == 'B':
+            br, bc = i, j
 
-Q = deque([(red[0], red[1], blue[0], blue[1], 0)])
+visit = [[[[-1]*m for _ in range(n)] for _ in range(m)] for _ in range(n)]
+visit[rr][rc][br][bc] = 0
+
+Q = deque([(rr, rc, br, bc)])
 
 while Q:
-    rr, rc, br, bc, cnt = Q.popleft()
+    ri, rj, bi, bj = Q.popleft()
+
+    if field[ri][rj] == 'O':
+        print(visit[ri][rj][bi][bj])
+        exit()
+
+    if visit[ri][rj][bi][bj] >= 10:
+        continue
 
     for d in direct:
-        p = 1
-        x, y = rr+d[0]*p, rc+d[1]*p
-        while 0 < x < n-1 and 0 < y < m-1 and arr[x][y] != '#':
-            if [x, y] == hole:
+        # 빨간 구슬 이동
+        nri, nrj = ri, rj
+        while True:
+            nri += d[0]
+            nrj += d[1]
+            if field[nri][nrj] == '#':
+                nri -= d[0]
+                nrj -= d[1]
                 break
-            p += 1
-            x, y = rr+d[0]*p, rc+d[1]*p
-
-        if p != 1:
-            rr, rc = x, y
-
-        p = 1
-        x, y = br+d[0]*p, bc+d[1]*p
-        while 0 < x < n-1 and 0 < y < m-1 and arr[x][y] != '#':
-            if [x, y] == hole:
+            if field[nri][nrj] == 'O':
                 break
-            p += 1
-        if p != 1:
-            br, bc = x, y
+
+        # 파란 구슬 이동
+        nbi, nbj = bi, bj
+        while True:
+            nbi += d[0]
+            nbj += d[1]
+            if field[nbi][nbj] == '#':
+                nbi -= d[0]
+                nbj -= d[1]
+                break
+            if field[nbi][nbj] == 'O':
+                break
+
+        # 파란 구슬이 구멍에 빠지면 continue
+        if field[nbi][nbj] == 'O':
+            continue
+
+        # 두개 위치가 같다면 늦게 이동한 구슬이 한칸 뒤로 이동
+        if nri == nbi and nrj == nbj:
+            if abs(nri - ri) + abs(nrj - rj) > abs(nbi - bi) + abs(nbj - bj):
+                nri -= d[0]
+                nrj -= d[1]
+            else:
+                nbi -= d[0]
+                nbj -= d[1]
+
+        if visit[nri][nrj][nbi][nbj] == -1:
+            visit[nri][nrj][nbi][nbj] = visit[ri][rj][bi][bj] + 1
+            Q.append((nri, nrj, nbi, nbj))
+else:
+    print(-1)
